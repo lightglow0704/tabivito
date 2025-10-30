6da493d5077cd91016bb9adebde2a0ec47d154ca
wjddbs0524 <sk40408981@gmail.com>
2025-10-30 16:48:39 +0900

    안쓰는 table 삭제

--- backend/sql/tabivito_db.sql
+++ backend/sql/tabivito_db.sql
@@ -10,15 +10,6 @@ create table users (
   email VARCHAR(100)
 );
 
-CREATE TABLE favorites (
-   favorite_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-   user_id BIGINT NOT NULL,
-   travel_name VARCHAR(255) NOT NULL,
-   travel_subtitle VARCHAR(255),
-   travel_image VARCHAR(255),
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   UNIQUE(user_id, travel_name)
-);
 
 select * from users;
 
